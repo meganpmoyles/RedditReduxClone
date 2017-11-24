5735ffa69aabb3343710aadc0400fea6930e6372
Siddharth Chauhan <csiddha@elemental.com>
2017-11-24 12:24:42 -0800

    Resolve compile errors

---
+++ src/store/configureStore.js
@@ -0,0 +1,5 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+const loggerMiddleware = createLogger()
+
