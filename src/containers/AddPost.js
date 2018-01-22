345103a7a686de98337f0ac283c3089bbf2f6cf2
Chauhan <csiddha@acbc32852ccf.ant.amazon.com>
2018-01-21 18:48:54 -0800

    Add Post UI

--- src/containers/AddPost.js
+++ src/containers/AddPost.js
@@ -10,21 +10,25 @@ class AddPost extends Component {
 
         handleSubmit(event) {
           event.preventDefault();
-          const data = new FormData(event.target);
+          const data = this.state;
           this.props.addPost(data, "all");
+        }
+
+        handleChange = (e) => {
+          this.setState({[e.target.name]: e.target.value});
         }
 
         render() {
           return (
-            <form onSubmit={this.handleSubmit}>
+            <form onChange={this.handleChange} onSubmit={this.handleSubmit}>
               <label htmlFor="title">Title</label>
-              <input id="title" name="title" type="text" />
+              <input id="title" name="title" type="text"  onChange={this.handleChange} />
               <label htmlFor="body">Post</label>
-              <input id="body" name="body" type="text" />
+              <input id="body" name="body" type="text"  onChange={this.handleChange} />
               <label htmlFor="author">Author</label>
-              <input id="author" name="author" type="text" />
+              <input id="author" name="author" type="text" onChange={this.handleChange}  />
               <label htmlFor="category">Category</label>
-              <input id="category" name="category" type="text" />
+              <input id="category" name="category" type="text"  onChange={this.handleChange} />
               <button type="submit">Add Post</button>
             </form>
           );
